a3086e8a971127ddc1f222b4443dfb17847c7604
MatheusGrigio14 <mgsilva14@stefanini.com>
2024-03-02 11:18:33 -0300

    Week 02 preparation.

---
+++ prepare/Learning02/Models/Job.cs
@@ -0,0 +1,15 @@
+﻿namespace Learning02.Models
+{
+    public class Job
+    {
+        public string _company { get; set; }
+
+        public string _jobTitle { get; set; }
+
+        public int _startYear { get; set; }
+
+        public int _endYear { get; set; }
+
+        public void Display() => Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
---
+++ prepare/Learning02/Models/Resume.cs
@@ -0,0 +1,17 @@
+﻿namespace Learning02.Models
+{
+    public class Resume
+    {
+        public string _name { get; set; }
+
+        public List<Job> _jobs { get; set; } = new();
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine("Jobs: ");
+
+            _jobs.ForEach(x => x.Display());
+        }
+    }
+}
