4f5a5e848e2136c5d39f75418c28fffbefbf87c9
MatheusGrigio14 <mgsilva14@stefanini.com>
2024-03-13 20:09:00 -0300

    Learning activity of week 03.

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,41 @@
+﻿namespace Learning03
+{
+    public class Fraction
+    {
+        private int _top;
+
+        private int _bottom;
+
+        public int GetTop() => _top;
+
+        public void SetTop(int top) => _top = top;
+
+        public int GetBottom() => _bottom;
+
+        public void SetBottom(int bottom) => _bottom = bottom;
+
+        public Fraction()
+        {
+            _top = 1;
+            _bottom = 1;
+        }
+
+        public Fraction(int wholeNumber)
+        {
+            _top = wholeNumber;
+            _bottom = 1;
+        }
+
+        public Fraction(int top, int bottom)
+        {
+            _top = top;
+
+            if(bottom != 0)
+                _bottom = bottom;
+        }
+
+        public string GetFractionString() => $"{_top}/{_bottom}";
+
+        public double GetDecimalValue() => (double) _top / _bottom;
+    }
+}
